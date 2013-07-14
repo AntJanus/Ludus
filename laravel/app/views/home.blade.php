9522eb6ecebe3a431f49b0418dd5376c9ede2a99
Antonin Januska <antonin@antjanus.com>
2013-07-14 10:28:41 -0500

    Basic layout

--- laravel/app/views/home.blade.php
+++ laravel/app/views/home.blade.php
@@ -2,10 +2,42 @@
 
 @section('content')
 
-Yay!
+<h3>Feed List</h3>
+<ul class="block-list feed-list">
+	<li>
+		<div class="feed-item"><h4>Title</h4>
+			Description of all kinds
+			<ul class="list-meta">
+				<li><a href="#">Upvote</a></li>
+				<li><a href="#">3 clicks</a></li>
+				<li><a href="#">Save</a></li>
+				<li><a href="#">Comment</a></li>
+			</ul>
+		</div>
+	</li>
+	<li>
+		<div class="feed-item"><h4>Title</h4>
+			Description of all kinds
+			<ul class="list-meta">
+				<li><a href="#">Upvote</a></li>
+				<li><a href="#">3 clicks</a></li>
+				<li><a href="#">Save</a></li>
+				<li><a href="#">Comment</a></li>
+			</ul>
+		</div>
+	</li>
+</ul>
 
 @stop
 
 @section('sidebar')
-Side!
+<ul class="block-list">
+	<li><h4>Title</h4>
+		Description of all kinds	
+	</li>
+	<li>
+		<h4>Title</h4>
+		Description of all kinds
+	</li>
+</ul>
 @stop
