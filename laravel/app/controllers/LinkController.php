84f0dbc713afd9703430d6a5debe6cbb8cdf7b0e
Antonin Januska <antonin@antjanus.com>
2013-10-16 15:00:11 -0500

    Redirect issues, URL building, checking for correct canonical link

--- laravel/app/controllers/LinkController.php
+++ laravel/app/controllers/LinkController.php
@@ -15,14 +15,21 @@ class LinkController extends \BaseController {
         return View::make('links.single')->with(array('link' => $link));
     }
 
-    public function linkCanon($slug)
+    public function linkCanon($category, $subcategory, $slug)
     {
         $link = Link::where('slug', '=', $slug)->first();
+        $cat = Category::where('id', '=', $link->category_id)->first();
+        $subcat = Category::where('id', '=', $link->subcategory_id)->first();
 
-        return View::make('links.single')->with(array(
-            'category' => $category,
-            'subcategory' => $subcategory,
-            'link' => $link));
+        if($cat->slug != $category || $subcat->slug != $subcategory){
+            return Redirect::to('links/'.$category->slug.'/'.$subcategory->slug.'/'.$link->slug);
+        }
+        else{
+            return View::make('links.single')->with(array(
+                'category' => $cat,
+                'subcategory' => $subcat,
+                'link' => $link));
+        }
     }
 
     public function linkAdd()
@@ -51,16 +58,19 @@ class LinkController extends \BaseController {
          *  * []return to URL page
          */
         $input  = Input::all();
-        $subcat = Category::find($input['category']);
 
         //check for duplicate
-        $linkCheck = Link::where('url', '=', $input['url'])->get();
-        if ($linkCheck == null) {
+        $linkCheck = Link::where('url', '=', $input['url'])->first();
+        if (empty($linkCheck)) {
 
             $link   = new Link;
             $tempSlug = Str::slug($input['title']);
             $slugCheck = Link::whereRaw("slug REGEXP '^{$tempSlug}(-[0-9]*)?$'")->count();
             $slug = $slugCheck > 0 ? $tempSlug.'-'.$slugCheck : $tempSlug;
+
+            //create url
+            $subcat = Category::find($input['category']);
+            $cat    = Category::find($subcat->parent);
 
             $link->fill(array(
                 'title' => Input::get('title'),
@@ -69,12 +79,19 @@ class LinkController extends \BaseController {
                 'subcategory_id' => $subcat->id,
                 'slug' => $slug
                 ));
+
             $link->save();
 
-            return Redirect::route('/links/'.$link->slug);
+            $url = 'links/'.$cat->slug.'/'.$subcat->slug.'/'.$link->slug;
+            echo $url; exit;
+            return Redirect::to($url);
         }
         else{
-            return Redirect::route('/links/'.$linkCheck->slug);
+            //create url
+            $subcat = Category::find($linkCheck->subcategory_id);
+            $cat    = Category::find($subcat->parent);
+            $url    = 'links/'.$cat->slug.'/'.$subcat->slug.'/'.$linkCheck->slug;
+            return Redirect::to($url);
         }
     }
 
